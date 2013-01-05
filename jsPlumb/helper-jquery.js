4c4bd5a21abcd0b03423ba7a3cdcc1332c572780
SamZ <admin@lndb.info>
2013-01-05 20:29:21 +0200

    Initial commit

---
+++ jsPlumb/helper-jquery.js
@@ -0,0 +1,37 @@
+/*
+ *  This file contains the JS that handles the switching between render modes.
+ */
+jsPlumb.bind("ready", function() {
+
+	jsPlumb.List.init();
+
+	// chrome fix.
+	document.onselectstart = function () { return false; };				
+
+    // render mode
+	var resetRenderMode = function(desiredMode) {
+		var newMode = jsPlumb.setRenderMode(desiredMode);
+		$(".rmode").removeClass("selected");
+		$(".rmode[mode='" + newMode + "']").addClass("selected");		
+
+		$(".rmode[mode='canvas']").attr("disabled", !jsPlumb.isCanvasAvailable());
+		$(".rmode[mode='svg']").attr("disabled", !jsPlumb.isSVGAvailable());
+		$(".rmode[mode='vml']").attr("disabled", !jsPlumb.isVMLAvailable());
+
+		//var disableList = (newMode === jsPlumb.VML) ? ",.rmode[mode='svg']" : ".rmode[mode='vml']"; 
+		//$(disableList).attr("disabled", true);				
+		jsPlumbChar.init();
+	};
+     
+	$(".rmode").bind("click", function() {
+		var desiredMode = $(this).attr("mode");
+		if (jsPlumbChar.reset) jsPlumbChar.reset();
+		jsPlumb.reset();
+		resetRenderMode(desiredMode);
+	});
+
+	// explanation div is draggable
+	$("#explanation,.renderMode").draggable();
+
+	resetRenderMode(jsPlumb.SVG);
+});
